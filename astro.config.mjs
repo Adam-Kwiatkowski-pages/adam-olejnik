14699c374f00f5632ffb20ba28de75514feeecb1
Adam Kwiatkowski <adam.kwiatkowski256@gmail.com>
2026-08-20 22:40:08 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 
-// TODO: Set SITE to the real domain (origin only) and BASE to undefined
-// TODO: Create public/CNAME file with domain name (without https://)
-const SITE = "https://adam-kwiatkowski-pages.github.io"
-const BASE = "/adam-olejnik"
+const SITE = "https://olejnik-robotyziemne.pl"
+const BASE = undefined
 
 export default defineConfig({
   site: SITE,
